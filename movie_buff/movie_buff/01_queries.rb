e99cf0d45fe95436b574132b3c0012a3857ec115
alberto <alyoung1991@gmail.com>
2021-09-10 14:35:46 -0400

    Worked up to directed_by_one_of

--- movie_buff/movie_buff/01_queries.rb
+++ movie_buff/movie_buff/01_queries.rb
@@ -22,6 +22,11 @@ def harrison_ford
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
   #Movie.joins(:actors).where()
+  Movie
+    .select(:id, :title)
+    .joins(:actors)
+    .where(actors: { name: 'Harrison Ford' })
+    .where.not(castings: { ord: 1 })
 end
 
 def biggest_cast
@@ -38,7 +43,13 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
-
+  Movie
+    .select(:id, :title)
+    .joins(:actors)
+    .group('movies.id')
+    .order('COUNT(actors.id) DESC')
+    .limit(3)
+    
 end
 
 def directed_by_one_of(them)
@@ -53,7 +64,10 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
-
+  Movie
+    .select(:id, :title)
+    .joins(:director)
+    .where(actors: {name: them})
 end
 
 def movie_names_before_1940
